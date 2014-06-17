e951d901faebbc58cf9293861bcd3b3e0ed17706
Snuggert <abe.wiersma@hotmail.nl>
2014-06-17 14:18:02 +0200

    nice kleurtjes

--- tracker/fill_db.py
+++ tracker/fill_db.py
@@ -7,7 +7,6 @@ the system.
 """
 from app import db
 from app.models import *
-from werkzeug.security import generate_password_hash
 
 #Main account
 admin = Account('root', 'n3tb0t')
@@ -15,5 +14,6 @@ db.session.add(admin)
 db.session.commit()
 
 peer1 = Peer('77.249.206.212')
+peer1.active = True
 db.session.add(peer1)
 db.session.commit()
